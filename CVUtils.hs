3686a6da07bed2db0a78b0270856106bdbd26d60
Tom Nielsen <tanielsen@gmail.com>
2011-09-05 16:16:12 +0100

    this at least works for tracking hidden woodlice

--- CVUtils.hs
+++ CVUtils.hs
@@ -41,8 +41,8 @@ data Obj =
 track1 bgIm objs im = 
    samplingImportanceResampling $ particleLike bgIm im objs -}
 
-sdv = 0.2
-sdvrot = 0.1
+sdv = 3
+sdvrot = 0.3
 evolve :: Obj -> Sampler (Obj,Obj)
 evolve o@(Obj vx vy vrot x y rot) = do
         nvx <- gaussD vx sdv
@@ -52,33 +52,39 @@ evolve o@(Obj vx vy vrot x y rot) = do
 
 type Square = (Pos,Pos)
 
-within :: Square -> R -> R -> Bool
 within ((lox,loy), (hix,hiy)) ox oy
        =    ox > min lox hix
          && ox < max lox hix
          && oy > min loy hiy
          && oy < max loy hiy
 
-invisible = [ ((963,627), 
-               (999,653))]
+invisible :: [((Int,Int),(Int,Int))]
 
-visible x y  = not $ any (\sqr -> within sqr (realToFrac x) (realToFrac y)) invisible
+invisible = [ ((963,626), 
+               (999,663))]
 
-nogo, invisible :: [Square]
+visible :: Int -> Int -> Bool
+visible x y  = not $ any (\sqr -> within sqr  x  y) invisible
+
+objCentreInvisible :: Obj -> Bool
+objCentreInvisible (Obj _ _ _ cx cy _) = not $ visible (round cx) (round cy)
+
+nogo::  [Square]
 nogo = 
-  [ ((963,627), (994,653))]
+  [ ((963,626), 
+     (997,663))]
 
 nogoPrior :: Obj -> R
 nogoPrior o = sum $ map f nogo
-          where  f sqr | within sqr (posx o) (posy o) = -1e10
+          where  f sqr | within sqr (posx o) (posy o) = -1e100
                        | otherwise = 0
 
 prevprior :: Obj -> (Obj -> R)
 prevprior (Obj vx vy vrot _ _ _) 
       (Obj nvx nvy nvrot _ _ _) 
-        =   PDF.gaussD vx sdv nvx
-          + PDF.gaussD vy sdv nvy
-          + PDF.gaussD vrot sdvrot nvy
+        =   PDF.gaussD vx (sdv/2) nvx -- "heavy tailed proposals"
+          + PDF.gaussD vy (sdv/2) nvy
+          + PDF.gaussD vrot (sdvrot/2) nvy
 
 fileroot = reverse . takeWhile (/='/') . reverse . takeWhile (/='.')
 
@@ -97,12 +103,20 @@ track sp bgIm vidfnm startfrm nframes obj0 = do
            diffObjs <- sample $ mapM evolve objs
            lift $ print2  "pixel1Red = " (frame!(0,0,0))
            let wparticles = dropLosers $ particleLike sp bgIm frame diffObjs
-           let smws = sumWeights wparticles
-           let cummSmws = cummWeightedSamples wparticles
+           let anyInvisible = any objCentreInvisible $ map fst wparticles
+           let npart = if anyInvisible then 5*nparticles else nparticles
+           let wparticles' = if anyInvisible then map (\(x,w) -> (x,w/5)) wparticles else wparticles
+
+           let smws = sumWeights wparticles'
+           let cummSmws = cummWeightedSamples wparticles'
+           lift $ print2 "npart=" npart          
+           lift $ print2 "nwinners= " (length wparticles')
+           lift $ hFlush stdout
            nextObjs <- sample $ sequence 
-                              $ replicate nparticles
+                              $ replicate npart
                               $ do u <- unitSample
                                    return . fst . fromJust $ find ((>=u*smws) . snd) cummSmws
+           --lift $ print $ map snd $ take 10 $ reverse $ cummSmws
 
            let mobj 
                  = runStat (pure Obj <*> before meanF velx
@@ -124,30 +138,31 @@ track sp bgIm vidfnm startfrm nframes obj0 = do
           
 print2 x y = putStrLn $ x ++show y
 
-smallest ws = foldl' (\acc (v,logpdf) -> min acc logpdf) (snd $ head ws) ws
+smallest ws = foldl1' (min) $ map snd ws
 largest ws = map snd $ sortBy (comparing snd) ws
 sumWeights ws = sum $ map (exp . (subtr (smallest ws)) . snd) ws
-cummWeightedSamples ws= scanl (\(_,csum) (x,w) -> (x,csum+exp (w-smallest ws))) (undefined,0) $ sortBy (comparing snd) ws
+cummWeightedSamples ws= 
+   let least = smallest ws
+   in scanl (\(_,csum) (x,w) -> (x,csum+exp (w-least))) (undefined,0) ws
 
 dropLosers ws = 
-  let srt = sortBy (comparing snd) ws
-      topW = snd $ last srt
+  let topW = foldl1' (max) $ map snd ws
       p (x,w) = w>topW-100
-  in filter p srt
+  in sortBy (comparing snd) $ filter p ws
 
 subtr x y = y - x
-nparticles = 1000
+nparticles = 500
 
-maxOn f xs = ceiling $ foldr1 max $ map f xs
-minOn f xs = floor $ foldr1 min $ map f xs
+maxOn f xs = ceiling $ foldl1' max $ map f xs
+minOn f xs = floor $ foldl1' min $ map f xs
  
 data StaticParams = SP {noise :: !R,
-                        length :: !R,
+                        wlength :: !R,
                         eccentric :: !R } deriving Show
 
 particleLike :: StaticParams -> Image -> Image -> [(Obj,Obj)] -> [(Obj,R)]
 particleLike sp@(SP noise len ecc) bgim im objprs = map pL objprs where
-  radiusi = 2* ceiling (len*ecc) + 10
+  radiusi = 2* ceiling (len*ecc) + 2
   objs = map snd objprs
   xs = [minOn (posx) objs - radiusi..maxOn (posx) objs+radiusi ] -- calc region of interest from all objs
   ys = [minOn (posy) objs-radiusi..maxOn posy objs+radiusi]
@@ -156,17 +171,19 @@ particleLike sp@(SP noise len ecc) bgim im objprs = map pL objprs where
         f1y = cy+(len*ecc)*sin rot
         f2x = cx-(len*ecc)*cos rot
         f2y = cy-(len*ecc)*sin rot
-        f rot cx cy x y ch 
-         = if (dist (f1x,f1y) (x, y) + dist (f2x,f2y) (x, y) < 2 * len) && visible x y
-              then gaussRnn noise 0 $ im!(y,x,ch)
-              else gaussW8nn noise (bgim!(y,x,ch)) $ im!(y,x,ch)
-    in (o, nogoPrior o + prevprior old o + sum [ f rot cx cy x y chan | 
+        f rot cx cy x y  
+          | not $ visible x y = 0
+          | dist  f1x f1y   x  y  + dist  f2x f2y   x  y  < 2 * len 
+               =  {-# SCC "gaussrn" #-} (gaussRnn noise 0 $ im!(y,x,0)) + (gaussRnn noise 0 $ im!(y,x,1)) + (gaussRnn noise 0 $ im!(y,x,2))
+          | otherwise = 
+              {-# SCC "gaussw8rn" #-} (gaussW8nn noise (bgim!(y,x,0)) $ im!(y,x,0))+ (gaussW8nn noise (bgim!(y,x,1)) $ im!(y,x,1)) 
+                   +(gaussW8nn noise (bgim!(y,x,2)) $ im!(y,x,2))
+    in (o, nogoPrior o + prevprior old o + {-# SCC "fsum" #-} (sum [ f rot cx cy x y  | 
                    x <- xs, 
-                   y <- ys, 
-                   chan <- [0..2]])
+                   y <- ys]))
 
-dist :: (R, R) -> (Int,Int) -> R 
-dist (cx,cy) (x,y) = sqrt((cx-realToFrac x)^2+(cy-realToFrac y)^2)
+dist :: R -> R -> Int ->Int -> R 
+dist  cx cy   x y  = sqrt((cx-(realToFrac x+0.5))^2+(cy-(realToFrac y+0.5))^2)
 
 gaussW8 :: R -> Word8 -> Word8 -> R
 gaussW8 tau muw8 = lpdf . word8ToR  
@@ -189,9 +206,9 @@ gaussRnn tau mu = lpdf . word8ToR
 
 
 word8ToR :: Word8 -> Double
-word8ToR = (/256) . realToFrac . fromEnum
+word8ToR = (/256) . realToFrac 
 realToW8 :: Double -> Word8
-realToW8 = toEnum . round. (*256) 
+realToW8 =  round. (*256) 
 
 markObjsOnImage :: [Obj] -> Image -> IO (Image)
 markObjsOnImage objs im = do
@@ -203,6 +220,8 @@ markObjsOnImage objs im = do
 
     freeze (mutIm::MImage)
     
+
+
          
 mkRed arr cx cy = do 
           writeArray arr ( cy,
@@ -215,32 +234,33 @@ mkRed arr cx cy = do
 main = do
      ilInit
      bgnm : fvid : (read -> x) : (read -> y) : (read -> rot) :_ <- getArgs
-     system $ "~/cvutils/extract "++fvid++" 1"
+     --system $ "~/cvutils/extract "++fvid++" 1"
      bgIm <-readImage bgnm
-     frame0 <-readImage "extract.png"
+     --frame0 <-readImage "extract.png"
      {-marked <- markObjsOnImage [Obj (x,y) 0, 
                                 Obj (x+1,y) 0, 
                                 Obj (x,y+1) 0, 
                                 Obj (x+1,y+1) 0] frame0
      writeImage "marked.png" marked -}
-     let posterior = posteriorV bgIm frame0 (round x,round y)
-         postAndV v = (posteriorV bgIm frame0 (round x,round y) $ fromList v, v)
+     let --posterior = posteriorV bgIm frame0 (round x,round y)
+         --postAndV v = (posteriorV bgIm frame0 (round x,round y) $ fromList v, v)
          sp = (SP 218 6 0.9)
 --         rot = negate (pi/7)
-         initialsV = fromList [x,y,218, 6, rot, 0.9]
+         --initialsV = fromList [x,y,218, 6, rot, 0.9]
          initObj =  (Obj 0 0 0 x y rot) 
-     ellim <- markEllipse sp initObj frame0
-     writeImage "markell.png" ellim
-     print $ postAndV [x,y,216, 6, rot, 0.9]
-     print $ postAndV [x,y,218, 6, rot, 0.9]
-     print $ postAndV [x,y,220, 6, rot, 0.9]
+     --ellim <- markEllipse sp initObj frame0
+     --writeImage "markell.png" ellim
+     print $ nogoPrior (Obj 0 0 0 992 650 rot)
+     print $ nogoPrior (Obj 0 0 0 997 650 rot)
+     print $ visible 994 650
+     print $ visible 1000 650
 
      runRIO $ do
 --         iniampar <- sample $ initialAdaMet 500 1e-4 posterior initialsV
 --         AMPar v _ _ _ <- runAndDiscard 5000 (show . ampPar) iniampar $ adaMet False posterior
 --         lift $ print v
 --         track (v@> 2) (v @> 3) bgIm fvid 3 (v@>0,v@>1)
-         track sp bgIm fvid 1020 200 $ initObj
+         track sp bgIm fvid 1020 50 $ initObj
      
      return () 
 
@@ -268,7 +288,7 @@ posteriorV bgim im (cx,cy) v =
         f2y = py-(len*ecc)*sin rot
         f :: Int -> Int -> Int -> R
         f x y ch 
-         = if dist (f1x,f1y) (x, y) + dist (f2x,f2y) (x, y) < 2 * len
+         = if dist  f1x f1y   x  y  + dist  f2x f2y   x  y  < 2 * len
               then gaussR noise 0 $ im!(y,x,ch)
               else gaussW8 noise (bgim!(y,x,ch)) $ im!(y,x,ch)
 
@@ -282,14 +302,19 @@ markEllipse sp@(SP noise len ecc) (Obj _ _ _ px py rot) im = do
         f1y = py+(len*ecc)*sin rot
         f2x = px-(len*ecc)*cos rot
         f2y = py-(len*ecc)*sin rot
-    print sp
-    print (f1x, f1y)
-    print (f2x, f2y)
-    print $ dist (f1x,f1y) (cx, cy)
+    forM [627, 630..663] $ \y-> do
+       writeArray mutIm (y, 996, 2) 255
+       writeArray mutIm (y, 999, 2) 255
+       writeArray mutIm (y, 963, 2) 255
+--    print sp
+--    print (f1x, f1y)
+--    print (f2x, f2y)
+--    print $ dist (f1x,f1y) (cx, cy)
     let f x y = do -- print2 "f at" (x,y,dist (f1x,f1y) (x, y) + dist (f2x,f2y) (x, y) )
-                   when (dist (f1x,f1y) (x, y) + dist (f2x,f2y) (x, y) < 2 * len) $ do
+                   when (dist  f1x f1y   x  y  + dist  f2x f2y   x  y  < 2 * len) $ do
                       --print2 "red at " (x,y) 
-                      mkRed mutIm x y
+                      --mkRed mutIm x 
+                      return ()
     sequence_ [ f x y  | 
                    x <- [ (cx::Int) -50.. cx +50],
                    y <- [ cy -50.. cy +50]]
