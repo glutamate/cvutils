3820505c24d98c5158dda5987a2c8881a7405768
Tom Nielsen <tanielsen@gmail.com>
2011-09-09 04:25:30 +0800

    more edge

--- Edge.hs
+++ Edge.hs
@@ -16,11 +16,25 @@ import Baysig.Estimate.RTS
 import Data.Array.Unboxed
 import Numeric.LinearAlgebra hiding (find)
 import qualified Math.Probably.PDF as PDF
-import Data.List
+import Data.List hiding (map)
 import Data.Maybe
 import Control.Applicative
 import Data.Ord
 import Graphics.Triangulation.Triangulation
+import qualified Data.Vector as V
+import Graphics.Formats.Collada.Vector2D3D
+
+pairToV :: (Double,Double) -> V2
+pairToV (x,y) = V (realToFrac x) (realToFrac y)
+
+type Polygon = V.Vector V2
+
+toPoly :: [(Double,Double)] -> Polygon
+toPoly pts = V.fromList $ map pairToV pts
+
+inside :: Polygon -> (Double,Double)  -> Bool
+inside poly xy = pointInside (pairToV xy) poly
+
 
 loadPoints :: IO [(Double,Double)]
 loadPoints = fmap read $ readFile "edge.dat"
